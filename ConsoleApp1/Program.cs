612c4430a7c0d3aea32c6a2158509a6cee5a31b5
Mate Balogh <b.mate11@gmail.com>
2017-10-27 13:41:48 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Codecool
+{
+    class Person
+    {
+        String Name { get; set; }
+        DateTime BirthDate { get; set; }
+        Genders Gender { get; set; }
+
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+            person.Name = "Julie Smith";
+            person.Gender = Genders.FEMALE;
+            person.BirthDate = new DateTime(1983, 11, 3);
+
+            Console.WriteLine(person.ToString());
+            Console.ReadKey();
+        }
+
+        override
+            public String ToString()
+        {
+            return $"Name: {Name}, Birth date: {BirthDate.ToString("yyyy MM dd")}, Gender: {Gender}";
+        }
+    }
+
+    enum Genders
+    {
+        MALE,
+        FEMALE
+    }
+}
